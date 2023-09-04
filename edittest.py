702c0b042e183fcbdaa4e3612cf0d11647a86f7c
sjnq <1197440299@qq.com>
2023-09-04 18:28:39 +0900

    add file mod

---
+++ edittest.py
@@ -0,0 +1,21 @@
+a = "hello"
+b = "world"
+print(a, b)
+
+print(a)
+print(b)
+print(b ,a)
+print("python", a, b)
+
+number_school = 2394584903
+
+str_name = "python"
+
+bool_view = False
+
+flt_pi = 3.14
+
+char_element = 'a'
+
+chars = "hello world"
+print("number : ", number_school, " name : ",str_name = "bool : ", bool_view = "FLOAT : ", flt_pi, " charcter : " , char_element, " charcters : " chars) 
